21770db9410cbb9caee01adb3584515999e502d2
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-05-13 13:39:56 +0300

    MyCompressorOutputStream

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -18,26 +18,26 @@ public class MyCompressorOutputStream extends OutputStream {
     @Override
     public void write(byte[] b) throws IOException {
         // we need something smarter
-//        int count = 0;
-//        int bit = 0;
-//        for (byte currentByte : b) {
-//            for (int i = 0; i < 8; i++) {
-//                int currentBit = (currentByte >> i) & 1;
-//                if (currentBit == bit) {
-//                    count++;
-//                } else {
-//                    // Output the count and bit to the stream
-//                    out.write(count);
-//                    out.write(bit);
-//                    // Reset the count and bit for the next sequence
-//                    count = 1;
-//                    bit = currentBit;
-//                }
-//            }
-//        }
-//        // Output the final count and bit to the stream
-//        out.write(count);
-//        out.write(bit);
+        int count = 0;
+        int bit = 0;
+        for (byte currentByte : b) {
+            for (int i = 0; i < 8; i++) {
+                int currentBit = (currentByte >> i) & 1;
+                if (currentBit == bit) {
+                    count++;
+                } else {
+                    // Output the count and bit to the stream
+                    out.write(count);
+                    out.write(bit);
+                    // Reset the count and bit for the next sequence
+                    count = 1;
+                    bit = currentBit;
+                }
+            }
+        }
+        // Output the final count and bit to the stream
+        out.write(count);
+        out.write(bit);
 
     }
 }
