82f00fdcd1e58f3c3466966742de6a9d30ea7e78
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-05-20 14:21:27 +0300

    Finish part C

--- src/test/GeneralCheckingFunctions.java
+++ src/test/GeneralCheckingFunctions.java
@@ -11,7 +11,7 @@ public class GeneralCheckingFunctions {
     public static String getGithubLink(){
         //change the <username> in the link to the username of the student who
 //        created the github project:
-        String githubLink = "https://github.com/MazePart1-2023/ATP-Project-PartA.git";
+        String githubLink = "https://github.com/Maze-2023/ATP-Project-PartB.git";
         return githubLink;
     }
 }
