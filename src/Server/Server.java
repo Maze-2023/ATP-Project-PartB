0fdbcc21f7bf2e4028027c2219d9de5074ae63a6
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-05-20 14:35:48 +0300

    docstring and comments on servers

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * The Server class represents a server that listens for client connections and applies a server strategy.
+ */
 public class Server {
     private int port;
     private int listeningIntervalMS;
@@ -15,6 +17,12 @@ public class Server {
     private volatile boolean stop;
     private ExecutorService threadPool;
 
+    /**
+     * Constructs a new Server object with the specified port, listening interval, and server strategy.
+     * @param port                 The port number on which the server listens for client connections.
+     * @param listeningIntervalMS  The timeout interval for accepting client connections.
+     * @param strategy             The server strategy to apply for handling client requests.
+     */
     public Server(int port, int listeningIntervalMS, IServerStrategy strategy) {
         this.port = port;
         this.listeningIntervalMS = listeningIntervalMS;
@@ -22,48 +30,58 @@ public class Server {
         this.threadPool = Executors.newFixedThreadPool(Integer.parseInt(Configurations.getInstance().properties.getProperty("threadPoolSize")));
     }
 
-    public void start()
-    {
+    /**
+     * Starts the server by running it in a separate thread.
+     */
+    public void start() {
         new Thread(this::run).start();
-
     }
 
-    public void run(){
+    /**
+     * Runs the server and handles client connections.
+     */
+    private void run() {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(listeningIntervalMS);
             System.out.println("Starting server at port = " + port);
 
-            while(!stop){
-                try{
+            while (!stop) {
+                try {
                     Socket clientSocket = serverSocket.accept();
-                    System.out.println("Client accepted" + clientSocket.toString());
+                    System.out.println("Client accepted: " + clientSocket.toString());
+
+                    // Handle the client request in a separate thread from the thread pool
                     threadPool.execute(new Thread(() -> handleClient(clientSocket)));
-                }
-                catch (SocketTimeoutException e){
+                } catch (SocketTimeoutException e) {
                     System.out.println("Socket timeout");
                 }
+
+                // Shut down the thread pool after handling a single client connection
                 threadPool.shutdown();
-
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void handleClient(Socket clientSocket){
+    /**
+     * Handles a client connection by applying the server strategy to the client's input.
+     * @param clientSocket  The socket representing the client connection.
+     */
+    private void handleClient(Socket clientSocket) {
         try {
             strategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
             clientSocket.close();
-        }
-        catch (IOException o){
+        } catch (IOException o) {
             o.printStackTrace();
         }
     }
 
-
-    public void stop(){
+    /**
+     * Stops the server by setting the stop flag.
+     */
+    public void stop() {
         stop = true;
     }
 }
--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -7,6 +7,11 @@ import java.io.*;
 
 public class ServerStrategyGenerateMaze implements IServerStrategy {
 
+    /**
+     * Applies the server strategy to generate and send a compressed maze to the client.
+     * @param inFromClient  The input stream from the client.
+     * @param outToClient   The output stream to the client.
+     */
     @Override
     public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
         try {
@@ -14,7 +19,11 @@ public class ServerStrategyGenerateMaze implements IServerStrategy {
             ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
 
             int[] input = (int[]) fromClient.readObject();
+
+            // Create a compressed maze using the input size
             byte[] out = createCompressedMaze(input);
+
+            // Send the compressed maze to the client
             toClient.writeObject(out);
             toClient.flush();
             fromClient.close();
@@ -24,9 +33,16 @@ public class ServerStrategyGenerateMaze implements IServerStrategy {
         }
     }
 
+    /**
+     * Generates a maze using the configured algorithm and compresses it.
+     * @param size  The size of the maze to generate.
+     * @return      The compressed byte array representation of the generated maze.
+     */
     private byte[] createCompressedMaze(int[] size) {
         String algorithm = Configurations.getInstance().properties.getProperty("mazeGeneratingAlgorithm");
         AMazeGenerator mazeGenerator = null;
+
+        // Determine the maze generator based on the configured algorithm
         if(algorithm.equals("MyMazeGenerator"))
             mazeGenerator = new MyMazeGenerator();
         else if(algorithm.equals("EmptyMazeGenerator"))
@@ -35,16 +51,18 @@ public class ServerStrategyGenerateMaze implements IServerStrategy {
             mazeGenerator = new SimpleMazeGenerator();
 
         assert mazeGenerator != null;
+
+        // Generate the maze using the selected maze generator
         Maze maze = mazeGenerator.generate(size[0], size[1]);
+
+        // Compress the maze into a byte array
         ByteArrayOutputStream compressedOutput = new ByteArrayOutputStream();
         OutputStream out = new MyCompressorOutputStream(compressedOutput);
         try {
             out.write(maze.toByteArray());
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return compressedOutput.toByteArray();
-
     }
 }
